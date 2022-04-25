1bf80beacc3fb16311386418615851281d6a8dbd
Niklas <83658950+Northern-Sword@users.noreply.github.com>
2022-04-25 20:48:16 +0200

    add protocol route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProtocolComponent } from "./protocol/protocol.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'protocol', component: ProtocolComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
