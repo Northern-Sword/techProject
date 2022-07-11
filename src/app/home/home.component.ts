a6553518a2a9637a5dac4b33ca788bc370933c79
Niklas <83658950+Northern-Sword@users.noreply.github.com>
2022-07-11 18:05:24 +0200

    add open dialog function

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {DurationService} from "../duration.service";
+import {MatDialog} from "@angular/material/dialog";
+import {UserInputDialogComponent} from "../user-input-dialog/user-input-dialog.component";
 
 @Component({
   selector: 'app-home',
@@ -8,9 +10,17 @@ import {DurationService} from "../duration.service";
 })
 export class HomeComponent {
 
-  constructor(private duration: DurationService) { }
+  constructor(private duration: DurationService, public dialog: MatDialog) { }
 
   startTimer() {
     this.duration.startTimer();
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(UserInputDialogComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 }
